a8b5af04a6a4da7aa081cb023f844714294bf630
kiritimishra <kiritimishra@hotmail.com>
2017-08-19 22:47:54 +0530

    initial checkin
    initial checkin

---
+++ SignalRchat/SignalRchat/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(SignalRchat.Startup))]
+namespace SignalRchat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+
+    }
+}
